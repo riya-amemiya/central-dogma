4a1ede5a12b28240d9b83bdc669f778d09e9b8b2
Riya Amemiya <riyaamemiya@gmail.com>
2024-12-28 18:06:42 +0900

    feat: UUIDv7のバルク取得機能を追加

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,11 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 
 import { uuidV7Handler, uuidV7Route } from "./uuid-v7";
+import { uuidsV7Handler, uuidsV7Route } from "./uuids-v7";
 
 const appPages = new OpenAPIHono();
 
 appPages.openapi(uuidV7Route, uuidV7Handler);
+appPages.openapi(uuidsV7Route, uuidsV7Handler);
 
 export { appPages };
