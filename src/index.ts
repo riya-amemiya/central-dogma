c5a3655f4c98ff6c320f51a2dcae0d28f8667e86
Yuta Nishi <riyaamemiya@gmail.com>
2025-01-08 14:35:29 +0900

    feat: ドキュメントエンドポイントのキャッシュ制御とポート設定を追加

--- src/index.ts
+++ src/index.ts
@@ -22,22 +22,38 @@ app.get(
 
 app.route("/", appPages);
 
-app.doc31("/doc", {
-  openapi: "3.1.0",
-  info: {
-    version: "1.0.0",
-    title: "Central Dogma",
-  },
-  servers: [
-    {
-      url: "https://central-dogma.oshaburikitchin.com",
+const port = process.env.PORT === undefined ? 3000 : Number(process.env.PORT);
+
+app.doc31("/doc", (c) => {
+  c.header(
+    "Cache-Control",
+    `public, max-age=${convertTime(OneDayMs, "milliseconds", "seconds")}`,
+  );
+  return {
+    openapi: "3.1.0",
+    info: {
+      version: "1.0.0",
+      title: "Central Dogma",
     },
-  ],
+    servers: [
+      {
+        url:
+          port === 3000
+            ? "http://localhost:3000"
+            : "https://central-dogma.oshaburikitchin.com",
+      },
+    ],
+  };
 });
 
-app.get("/doc-ui", swaggerUI({ url: "/doc" }));
+app.get(
+  "/doc-ui",
+  cacheMiddleware({
+    directives: ["public"],
+  }),
+  swaggerUI({ url: "/doc" }),
+);
 
-const port = process.env.PORT === undefined ? 3000 : Number(process.env.PORT);
 // biome-ignore lint/style/noDefaultExport: <explanation>
 export default {
   port,
